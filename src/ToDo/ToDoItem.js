9ba9588bd455a0bd1f1f1e8dccb14700d146787c
Riddhi Damani <riddhidamani90@gmail.com>
2021-09-28 22:50:24 -0500

    Formatted ToDo list layout

--- src/ToDo/ToDoItem.js
+++ src/ToDo/ToDoItem.js
@@ -7,51 +7,42 @@ export default function ToDoItem({
   complete,
   dateCompleted,
 }) {
-  const tableStyle = {
-    border: "1px solid black",
-    borderCollapse: "collapse",
-    textAlign: "center",
-    width: "100%",
-  };
-
-  const tdStyle = {
-    border: "1px solid black",
-    background: "white",
-    padding: "5px",
-  };
-
   const [checked, setCheckBox] = useState(false);
 
-  
-
-  function handleChange(evt) {
+  function handleChange() {
     setCheckBox(!checked);
   }
 
   return (
     <div>
-      <table style={tableStyle}>
-        <tbody>
-          <tr>
-            <td>
-              <input
-                type="checkbox"
-                name="checkbox"
-                defaultChecked={checked}
-                onChange={handleChange}
-              />
-            </td>
-            <td style={tdStyle}>{title}</td>
-            <td style={tdStyle}>{description}</td>
-            <td style={tdStyle}>{dateCreated}</td>
-            <td style={tdStyle}>
-              {checked && (
-                <text> {Date(Date.now()).toString().slice(0, 25)} </text>
-              )}
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div>
+        <input
+          type="checkbox"
+          name="checkbox"
+          defaultChecked={checked}
+          onChange={handleChange}
+        />
+        <span>
+          <strong>Title: </strong>
+          {title}
+        </span>
+        <br />
+        <span>
+          <strong>Description: </strong>
+          {description}
+        </span>
+        <br />
+        <span>
+          <strong>Date Created:</strong> {dateCreated}
+        </span>
+        <br />
+        <span>
+          <strong>Date Completed:</strong>
+          {checked && <text> {Date(Date.now()).toString().slice(0, 25)} </text>}
+        </span>
+        <br />
+        <hr></hr>
+      </div>
     </div>
   );
 }
