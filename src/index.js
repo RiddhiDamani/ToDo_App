61911d122edf7798045b734adc6fa9e4b0f530a3
Riddhi Damani <riddhidamani90@gmail.com>
2021-10-21 00:37:09 -0500

    HW4 Q3 - Configured an Axios Instance and RequestProvider in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { RequestProvider } from "react-request-hook";
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:3000/api/",
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RequestProvider value={axiosInstance}>
+      <App />
+    </RequestProvider>
+    ,
   </React.StrictMode>,
   document.getElementById("root")
 );
