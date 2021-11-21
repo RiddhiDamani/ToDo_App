c59e112316615008e03aa42e8e373cc0781c0bfd
Riddhi Damani <riddhidamani90@gmail.com>
2021-11-21 10:45:51 -0600

    Added ToDo express logic

--- todo-mongo-server/models/Todo.js
+++ todo-mongo-server/models/Todo.js
@@ -5,8 +5,6 @@ const Schema = mongoose.Schema;
 const TodoSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  dateCreated: { type: Date, required: true },
-  dateCompleted: { type: Date, required: true },
   id: { type: Schema.Types.ObjectId, ref: "User" },
 });
 
--- todo-mongo-server/routes/todos.js
+++ todo-mongo-server/routes/todos.js
@@ -1,10 +1,51 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function (req, res, next) {});
+const Todo = require("../models/Todo");
+const jwt = require("jsonwebtoken");
+const privateKey = ``;
 
-router.post("/", function (req, res) {
-  res.send(req.params);
+// Router-level Middleware
+router.use(function (req, res, next) {
+  console.log(req.header("Authorization"));
+  if (req.header("Authorization")) {
+    try {
+      req.payload = jwt.verify(req.header("Authorization"), privateKey, {
+        algorithms: ["RS256"],
+      });
+    } catch (error) {
+      return res.status(401).json({ error: error.message });
+    }
+  } else {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+  next();
+});
+
+router.get("/", async function (req, res, next) {
+  const todos = await Todo.find().where("id").equals(req.payload.id).exec();
+  return res.status(200).json({ todos: todos });
+});
+
+router.post("/", async function (req, res) {
+  const todos = new Todo({
+    title: req.body.title,
+    description: req.body.description,
+    id: req.payload.id,
+  });
+
+  await todos
+    .save()
+    .then((savedTodo) => {
+      return res.status(201).json({
+        id: savedTodo._id,
+        title: savedTodo.title,
+        description: savedTodo.description,
+      });
+    })
+    .catch((error) => {
+      return res.status(500).json({ error: error.message });
+    });
 });
 
 module.exports = router;
